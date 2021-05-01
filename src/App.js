b67cae32adfdf99a17c668c04bb575f6d79127e1
TXLuong <tranxuanluong98@gmail.com>
2021-05-01 13:38:57 +0700

    inline-block

--- src/App.js
+++ src/App.js
@@ -3,9 +3,21 @@ import './App.css';
 
 function App() {
   return(
+	  <>
 	  <div className = "mydeployment">
 	  <h1>Hello! My name is Luong</h1>
 	  </div> 
+	  <ul className = "nav">
+	  <li> <a href = "#home"> Home </a> </li>
+	  
+	  <li> <a href = "#about"> About us </a> </li>
+	  
+	  <li> <a href = "#customers"> Our customers </a> </li>
+	  
+	  <li> <a href = "#contact">Contact Us </a> </li>
+	  </ul>
+	  </>
+
   );
 }
 
